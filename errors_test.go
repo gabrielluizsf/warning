c8ab022a9cdc6d3c4ea1bc6b77d45f37cad07b4d
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-24 11:54:24 -0300

    Removing test example description

--- errors_test.go
+++ errors_test.go
@@ -4,7 +4,6 @@ import (
   "os/exec"
   "fmt"
 )
-// This function test the PRINT_DEFAULT_ERRORS function
 func ExamplePRINT_DEFAULT_ERRORS(){
 	out, err := exec.Command("whoami").Output()
 	PRINT_DEFAULT_ERRORS(err, "INVALID COMMAND")
