ff78f8c69de9102e94e71e17f6626a53d1f7e768
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-25 15:00:22 -0300

    Benchmark test

--- errors_test.go
+++ errors_test.go
@@ -3,6 +3,7 @@ package warning
 import (
   "os/exec"
   "fmt"
+  "testing"
 )
 func ExamplePRINT_DEFAULT_ERRORS(){
 	out, err := exec.Command("whoami").Output()
@@ -12,4 +13,25 @@ func ExamplePRINT_DEFAULT_ERRORS(){
   //Output: 
   //[Command successfully executed]
  //gabriel
+}
+
+func BenchmarkPRINT_DEFAULT_ERRORS(benchmark *testing.B){
+  for i := 0; i < benchmark.N; i++{
+  out, err := exec.Command("disfhdisfias").Output();
+	PRINT_DEFAULT_ERRORS(err, "INVALID COMMAND");
+	output := string(out[:]);
+	fmt.Println(output);
+  }
+}
+func BenchmarkPRINT_DEFAULT_ERRORS_BASIC(benchmark *testing.B){
+  for i := 0; i < benchmark.N; i++{
+  out, err := exec.Command("disfhdisfias").Output();
+    if err != nil{
+      fmt.Println("INVALID COMMAND");
+    }else{
+      fmt.Println("Command successfully executed");
+    }
+	output := string(out[:]);
+	fmt.Println(output);
+  }
 }
