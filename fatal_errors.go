38289b56a47f032349315d255ea8c98aca356eac
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-26 13:06:37 -0300

    Adding Function description

--- fatal_errors.go
+++ fatal_errors.go
@@ -2,6 +2,7 @@ package warning
 
 import "log";
 
+//This function generates a log with the error message if there is an error
 func FATAL_ERROR(ERROR error){
   if ERROR != nil{
     log.Fatalln(ERROR);
