d0929d3785dac1eda0f6e54bb00ca2a7e588c262
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-30 13:46:27 -0300

    adding benchmarks

--- log_error_test.go
+++ log_error_test.go
@@ -57,3 +57,23 @@ func BenchmarkLOG_ERRORS_BASIC(benchmark *testing.B) {
     defer open.Close();
   }
 }
+
+func BenchmarkPANIC_ERROR(benchmark *testing.B) {
+  for i := 0; i < benchmark.N; i++{
+    filename := "file.adah";
+    open, err := os.Open(filename);
+      PANIC_ERROR(err);
+    defer open.Close();
+  }
+}
+
+func BenchmarkPANIC_ERROR_BASIC(benchmark *testing.B) {
+  for i := 0; i < benchmark.N; i++{
+    filename := "file.adah";
+    open, err := os.Open(filename);
+    if err != nil{
+      log.Panicln(err);
+    }
+    defer open.Close();
+  }
+}
