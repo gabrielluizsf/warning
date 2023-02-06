2a722314def1a75fbb5c6a2e8471569dc2e23f8f
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-02-06 14:27:42 -0300

    Updating Output

--- log_error_test.go
+++ log_error_test.go
@@ -20,21 +20,11 @@ func ExamplePANIC_ERROR(){
   open, err := os.Open(filename);
     PANIC_ERROR(err);
   defer open.Close();
-   //Output:
-   //2023/01/30 13:36:33 open file.adah: no such file or directory
+  //Output:
+  //2023/01/30 13:36:33 open file.adah: no such file or directory
   //panic: open file.adah: no such file or directory
-
-
-//goroutine 1 [running]:
-//log.Panicln({0xc00008ef48?, 0x9?, 0x0?})
-        //$HOME/.asdf/installs/golang/1.19.3/go/src/log/log.go:402 +0x65
-//github.com/theGOURL/warning.PANIC_ERROR(...)
-        //$HOME/projects/warning/log_error.go:16
-//main.Testing_the_PANIC_ERROR_function()
-        //$HOME/projects/warning/internal/main.go:21 +0x72
-//main.main()
-        //$HOME/projects/warning/internal/main.go:9 +0x17
-//exit status 2
+  //goroutine 1 [running]:
+ //exit status 2
 }
 
 func BenchmarkLOG_ERRORS(benchmark *testing.B) {
